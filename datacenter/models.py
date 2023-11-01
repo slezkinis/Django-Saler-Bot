2c28698620c45a24b250fe1a373a2e45ad001734
slezkinis <slezkin.is@mail.ru>
2023-11-01 23:20:05 +0300

    Add meta

--- datacenter/models.py
+++ datacenter/models.py
@@ -6,12 +6,21 @@ class Category(models.Model):
     name_ru = models.CharField('Название на русском', max_length=100)
     name_en = models.CharField('Название на английском', max_length=100)
 
+    class Meta:
+        verbose_name = 'Геолокация'
+        verbose_name_plural = 'Геолокации'
+
+
     def __str__(self) -> str:
         return self.name_ru
 
 
 class Source(models.Model):
     name = models.CharField('Название', max_length=100)
+
+    class Meta:
+        verbose_name = 'Источник'
+        verbose_name_plural = 'Источники'
 
     def __str__(self) -> str:
         return self.name
@@ -26,6 +35,10 @@ class Product(models.Model):
     price = models.DecimalField(verbose_name='Цена в $', max_digits=5, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='К какой категории привязан')
 
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+    
     def __str__(self) -> str:
         return self.name_ru
 
@@ -40,6 +53,10 @@ class User(models.Model):
     selected_language = models.CharField('Язык', choices=LANGUAGES, default='ru', max_length=100)
     money = models.DecimalField(verbose_name='Кол-во $', max_digits=5, decimal_places=2, default=0)
     
+    class Meta:
+        verbose_name = 'Покупатель'
+        verbose_name_plural = 'Покупатели'
+    
     def __str__(self) -> str:
         if self.username:
             return f'{self.username}_{self.tg_id}'
@@ -51,6 +68,10 @@ class Account(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='accounts', verbose_name='К какому товару привязан')
     is_enabled = models.BooleanField('Активен ли аккаунт', default=True)
     
+    class Meta:
+        verbose_name = 'Продаваемый аккаунт'
+        verbose_name_plural = 'Продаваемые аккаунты'
+
     def __str__(self) -> str:
         return f'{self.file.name}_{self.product.name_ru}'
 
@@ -71,6 +92,10 @@ class Order(models.Model):
         db_index=True
     )
 
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
     def __str__(self) -> str:
         return f'{self.user}_{self.account.file.name}_{self.status}'
 
@@ -79,6 +104,10 @@ class Order(models.Model):
 class Setting(models.Model):
     time_to_check = models.IntegerField('Время проверки аккаунта в минутах')
     _singleton = models.BooleanField(default=True, editable=False, unique=True)
+
+    class Meta:
+        verbose_name = 'Настройки'
+        verbose_name_plural = 'Настройки'
 
     def __str__(self) -> str:
         return 'Время для пользователя на проверку аккаунта'
